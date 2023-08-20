b68b446998bbdf09163c0b28480d7ec8e185f641
habid22 <habid22@uwo.ca>
2023-08-20 19:14:35 -0400

    hero section done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import { logo } from '../assets/assets';
 
 const Hero = () => {
   return (
-    <div>
-      Hero
-    </div>
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+        <img src= {logo} alt ="sumz_logo" className='w-28 object-contain' />
+      </nav>
+      <h1 className="head_text">
+        Summarize Articles with <br
+        className='max-md:hidden'/>
+        <span className='orange_gradient'>
+          OpenAI GPT-4
+        </span>
+      </h1>
+      <h2 className="desc">
+        Summarize any article with the power of OpenAI's GPT-4 model.
+      </h2>
+    </header>
   )
 }
 
