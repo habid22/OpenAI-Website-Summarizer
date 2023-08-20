3e9782f597f39ec766871d27a7e5759f0ff6e8f8
habid22 <habid22@uwo.ca>
2023-08-20 19:00:25 -0400

    f

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import Hero from './components/Hero.jsx';
+import Demo from './components/Demo.jsx';
+
+import './App.css';
 
 const App = () => {
   return (
-    <div> App </div>
+    <main>
+      <div className="main">
+       <div className="gradient" /> 
+      </div>
+
+    <div className="app">
+      <Hero />
+      <Demo />
+    </div>
+
+    </main>
+   
   )
 }
 
