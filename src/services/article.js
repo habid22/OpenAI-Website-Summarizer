9585b4a98f0b51c16c694fadb08dc144dad9383c
habid22 <habid22@uwo.ca>
2023-08-21 13:59:52 -0400

    f

--- src/services/article.js
+++ src/services/article.js
@@ -16,7 +16,6 @@ export const articleApi = createApi({
     endpoints: (builder) => ({
         getSummary: builder.query({
             query: (params) => `/summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`
-
        })
     })   
 });
