4de6ed197ce06091c7a8d4b15985c6a2fdc7e8b3
habid22 <habid22@uwo.ca>
2023-08-21 13:06:42 -0400

    handles

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -2,35 +2,52 @@ import { useState, useEffect } from 'react'
 import {copy, linkIcon, loader, tick} from '../assets/assets';
 
 const Demo = () => {
+  const [article, setArticle] = useState({
+    url: '',
+    summary: '',
+  });
+
+  const handleSubmit = async (e) => {
+    alert('Submitted');
+
+  }
+
+
+
   return (
    <section className='mt-16 w-full max-w-xl'>
     {/* Search */}
     <div className='flex flex-col w-full gap-2'>
       <form className= "relative flex justify-center items-center"
-       onSubmit={ () => {}}>
+       onSubmit={handleSubmit}
+       >
 
         <img src={linkIcon} alt="link_icon" className='absolute left-0 my-2 ml-3 w-5'/>
 
         <input type="text" 
-        placeholder="Paste a URL" value="" 
-        onChange={() => {}}
+        placeholder="Paste a URL" 
+        value={article.url}
+        onChange={(e) => setArticle({...
+          article, 
+          url: e.target.value})}
+        required
         className= "url_input peer"
         style={{ backgroundColor: '#3b4055',  borderColor: 'black' }}
 
         />
 
-<button
-  type="submit"
-  className="submit_btn peer-focus:text-gray-700 bg-purple-600 text-white px-4 py-2 rounded-lg ml-2 border-none flex items-center"
-  style={{ backgroundColor: "#6864f4" }}
->
-  <span className="mx-auto">➤</span>
-</button>
+      <button
+        type="submit"
+        className="submit_btn peer-focus:text-gray-700 bg-purple-600 text-white px-4 py-2 rounded-lg ml-2 border-none flex items-center"
+        style={{ backgroundColor: "#6864f4" }}
+      >
+        <span className="mx-auto">➤</span>
+      </button>
 
-      </form>
-      {/* Browser URL History*/}
-    </div>
-    {/* Display Results */}
+            </form>
+            {/* Browser URL History*/}
+          </div>
+          {/* Display Results */}
 
 
    </section>
