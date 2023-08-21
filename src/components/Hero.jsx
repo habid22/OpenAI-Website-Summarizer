d3b862c9f52882fb783f08d7df8d14af3c53a267
habid22 <habid22@uwo.ca>
2023-08-20 20:31:24 -0400

    f

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,20 +3,19 @@ import { logo } from '../assets/assets';
 const Hero = () => {
   return (
     <header className="w-full flex justify-center items-center flex-col">
-      <nav className='flex justify-between items-center w-full mb-10 pt-3'>
-        <img src= {logo} alt ="sumz_logo" className='w-28 object-contain' />
-      </nav>
-      <h1 className="head_text">
-        Summarize Articles with the <br
-        className='max-md:hidden'/>
-        <span className='orange_gradient'>
-          Power of AI.
-        </span>
-      </h1>
-      <h2 className="desc">
-        Summarize any article with the power of OpenAI's GPT-4 model.
-      </h2>
-    </header>
+  <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+    {/* <img src={logo} alt="sumz_logo" className='w-28 object-contain' /> */}
+  </nav>
+  <h1 className="head_text" style={{ color: 'white' }}>
+    Summarize Articles in just a  <br className='max-md:hidden' />
+    <span className='orange_gradient'>
+      Paste and Click.
+    </span>
+  </h1>
+  <h2 className="desc">
+    Summarize any article with the power of OpenAI's GPT-4 model.
+  </h2>
+</header>
   )
 }
 
